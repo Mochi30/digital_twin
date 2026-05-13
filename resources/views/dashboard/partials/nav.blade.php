718205b6665b5adca6071c15bd96a1a84ecb6a84
Moch. Azam <mochazam3002@gmail.com>
2026-05-13 09:55:26 +0700

    first commit

---
+++ resources/views/dashboard/partials/nav.blade.php
@@ -0,0 +1,6 @@
+<div class="nav-tabs">
+  <div class="nav-tab active" onclick="switchTab('overview')"><i class="ti ti-layout-dashboard" style="font-size:14px;margin-right:5px" aria-hidden="true"></i>Overview</div>
+  <div class="nav-tab" onclick="switchTab('detail')"><i class="ti ti-chart-line" style="font-size:14px;margin-right:5px" aria-hidden="true"></i>Detail titik</div>
+  <div class="nav-tab" onclick="switchTab('riwayat')"><i class="ti ti-history" style="font-size:14px;margin-right:5px" aria-hidden="true"></i>Riwayat</div>
+  <div class="nav-tab" onclick="switchTab('konfigurasi')"><i class="ti ti-settings" style="font-size:14px;margin-right:5px" aria-hidden="true"></i>Konfigurasi</div>
+</div>
